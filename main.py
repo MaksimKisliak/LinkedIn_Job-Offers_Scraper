40220f2c32230b477177b8ec9b547011f4cd7fec
MaksimKisliak <makskislyak@gmail.com>
2023-02-25 14:25:29 +0100

    Update the code to use OOP principles. Bug: ERROR - Error occurred while scraping job information for https://www.linkedin.com/jo..... : 'list' object has no attribute 'find_element'

--- main.py
+++ main.py
@@ -1,147 +1,136 @@
-import time
+import logging
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 import os
 
-# if "ON_HEROKU" in os.environ:
-if "RAILWAY_ENVIRONMENT" in os.environ:
-    # Setup for Railway
-    USER_NAME = os.environ.get("SECRET_KEY")
-    PASSWORD = os.environ.get("DATABASE_URL")
-else:
-    # Load from local .env
-    load_dotenv()
-    USER_NAME = os.environ.get("USER_NAME")
-    PASSWORD = os.environ.get("PASSWORD")
 
-driver = webdriver.Chrome()
+class LinkedInJobsScraper:
+    LOGIN_URL = 'https://www.linkedin.com/login'
+    JOB_SEARCH_URL = 'https://www.linkedin.com/jobs/search/?currentJobId=3477736514&geoId=105072130&keywords=python%20web%20developer&location=Poland&refresh=true'
 
-# Maximize Window
-driver.maximize_window()
-driver.switch_to.window(driver.current_window_handle)
-driver.implicitly_wait(10)
+    LOGIN_USERNAME_XPATH = '//input[@id="username"]'
+    LOGIN_PASSWORD_XPATH = '//input[@id="password"]'
+    LOGIN_BUTTON_XPATH = '//button[contains(text(),"Sign in")]'
 
-# Enter to the site
-driver.get('https://www.linkedin.com/login')
-time.sleep(2)
+    JOB_LIST_BLOCK_CLASSNAME = 'scaffold-layout__list-container'
+    JOB_LIST_ITEM_CLASSNAME = '.jobs-search-results__list-item'
+    JOB_ITEM_CLASSNAME = 'p5'
+    JOB_LINK_TAGNAME = 'a'
+    JOB_TITLE_TAGNAME = 'h1'
+    COMPANY_NAME_CLASSNAME = 'jobs-unified-top-card__company-name'
+    LOCATION_CLASSNAME = 'jobs-unified-top-card__bullet'
+    JOB_TYPE_CLASSNAME = 'jobs-unified-top-card__workplace-type'
+    POST_DATE_CLASSNAME = 'jobs-unified-top-card__posted-date'
+    JOB_DESC_CLASSNAME = 'jobs-box__html-content'
+    SHOW_MORE_BUTTON_XPATH = '//*[@id="ember33"]'
 
-# Accept cookies
-driver.find_element(By.XPATH, "/html/body/div/main/div[1]/div/section/div/div[2]/button[2]").click()
+    def __init__(self):
+        if "RAILWAY_ENVIRONMENT" in os.environ:
+            # Setup for Railway
+            user_name = os.environ.get("USER_NAME")
+            password = os.environ.get("PASSWORD")
+        else:
+            # Load from local .env
+            load_dotenv()
+            user_name = os.environ.get("USER_NAME")
+            password = os.environ.get("PASSWORD")
 
-# User Credentials
-driver.find_element(By.XPATH, '//*[@id="username"]').send_keys(USER_NAME)
-driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(PASSWORD)
-time.sleep(1)
+        self.username = user_name
+        self.password = password
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
 
-# Login button
-driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button').click()
-driver.implicitly_wait(30)
+    def login(self):
+        self.driver.maximize_window()
+        self.driver.get(self.LOGIN_URL)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, self.LOGIN_USERNAME_XPATH))).send_keys(self.username)
+        self.wait.until(EC.presence_of_element_located((By.XPATH, self.LOGIN_PASSWORD_XPATH))).send_keys(self.password)
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, self.LOGIN_BUTTON_XPATH))).click()
+        self.wait.until(EC.url_changes(self.LOGIN_URL))
 
-# Jobs page
-driver.get("https://www.linkedin.com/jobs/?")
-time.sleep(3)
-# Go to search results directly
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3477736514&geoId=105072130&keywords=python%20web%20developer&location=Poland&refresh=true")
-time.sleep(1)
+    def scrape_job_links(self):
+        job_links = []
+        for page in range(2, 3):
+            try:
+                self.wait.until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="ember13"]'))).click()  # Accept cookies
+                logging.info(f"Collecting job offer links just started on the page {page-1}")
+                self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, self.JOB_LIST_BLOCK_CLASSNAME)))
+                jobs_block = self.driver.find_element(By.CLASS_NAME, self.JOB_LIST_BLOCK_CLASSNAME)
+                logging.info(f"Scraped information for jobs_block: {jobs_block}")
+                jobs_list = jobs_block.find_elements(By.CSS_SELECTOR, self.JOB_LIST_ITEM_CLASSNAME)
+                logging.info(f"Scraped information for jobs_list: {jobs_list}")
+                for each_job in jobs_list[:3]:
+                    each_job_links = each_job.find_elements(By.TAG_NAME, self.JOB_LINK_TAGNAME)
+                    logging.info(f"Scraped information for {jobs_list}")
+                    for each_job_link in each_job_links:
+                        if str(each_job_link.get_attribute('href')).startswith("https://www.linkedin.com/jobs/view") and each_job_link.get_attribute('href') not in job_links:
+                            job_links.append(each_job_link.get_attribute("href"))
+                    self.driver.execute_script("arguments[0].scrollIntoView();", each_job)
+            except Exception as e:
+                logging.error(f'Error occurred while scraping job links: {e}')
+            self.driver.find_element(By.CSS_SELECTOR, f'button[aria-label="Page {page}"]').click()
+        logging.info(f'Found {len(job_links)} job offer links')
+        return job_links
 
-# Get all links for these offers
-links = []
-# Navigate 1 pages
-print('Links are being collected now.')
-try:
-    for page in range(2, 3):
-        time.sleep(2)
-        jobs_block = driver.find_element(By.CLASS_NAME,'scaffold-layout__list-container')
-        jobs_list = jobs_block.find_elements(By.CSS_SELECTOR, '.jobs-search-results__list-item')
-        print(f'page: {page}\njobs_block: {jobs_block}\njobs_list: {jobs_list} ')
-
-        for job in jobs_list:
-            all_links = job.find_elements(By.TAG_NAME,'a')
-            for a in all_links:
-                if str(a.get_attribute('href')).startswith("https://www.linkedin.com/jobs/view") and a.get_attribute(
-                        'href') not in links:
-                    links.append(a.get_attribute('href'))
-                    print(a.get_attribute('href'))
-                else:
-                    pass
-            # scroll down for each job element
-            driver.execute_script("arguments[0].scrollIntoView();", job)
-
-        print(f'Collecting the links in the page: {page - 1}')
-        # go to next page:
-        driver.find_element(By.XPATH, f"//button[@aria-label='Page {page}']").click()
-        time.sleep(3)
-except:
-    pass
-print('Found ' + str(len(links)) + ' links for job offers')
-
-# Create empty lists to store information
-job_titles = []
-company_names = []
-company_locations = []
-work_methods = []
-post_dates = []
-work_times = []
-job_desc = []
-
-i = 0
-j = 1
-# Visit each link one by one to scrape the information
-print('Visiting the links and collecting information just started.')
-# for i in range(len(links)):
-for i in range(2):
-    try:
-        driver.get(links[i])
-        i += 1
-        time.sleep(2)
-        # Click See more.
-        driver.find_element(By.ID, "ember33").click()
-        time.sleep(2)
-    except:
-        pass
-
-    # Find the general information of the job offers
-    contents = driver.find_elements(By.CLASS_NAME,'p5')
-    for content in contents:
+    def scrape_job_info(self, job_link):
+        job_data = {}
         try:
-            job_titles.append(content.find_element(By.TAG_NAME,"h1").text)
-            company_names.append(content.find_element(By.CLASS_NAME,"jobs-unified-top-card__company-name").text)
-            company_locations.append(content.find_element(By.CLASS_NAME,"jobs-unified-top-card__bullet").text)
-            work_methods.append(content.find_element(By.CLASS_NAME,"jobs-unified-top-card__workplace-type").text)
-            post_dates.append(content.find_element(By.CLASS_NAME,"jobs-unified-top-card__posted-date").text)
-            work_times.append(content.find_element(By.CLASS_NAME,"jobs-unified-top-card__job-insight").text)
-            print(f'Scraping the Job Offer {j} DONE.')
-            print(f'job_titles: {content.find_element(By.TAG_NAME,"h1").text}\n, '
-                  f'company_names: {content.find_element(By.CLASS_NAME,"jobs-unified-top-card__company-name").text}\n, '
-                  f'company_locations: {content.find_element(By.CLASS_NAME,"jobs-unified-top-card__bullet").text}\n, '
-                  f'work_methods: {content.find_element(By.CLASS_NAME,"jobs-unified-top-card__workplace-type").text}\n, '
-                  f'post_dates: {content.find_element(By.CLASS_NAME,"jobs-unified-top-card__posted-date").text}\n, '
-                  f'work_times: {content.find_element(By.CLASS_NAME,"jobs-unified-top-card__job-insight").text}\n, '
-                  )
-            j += 1
+            logging.info(f"Visiting the link and collecting information just started.")
+            self.driver.get(job_link)
+            self.wait.until(EC.element_to_be_clickable((By.XPATH, self.SHOW_MORE_BUTTON_XPATH))).click()  # Show More
+            logging.info(f"Cookies accepted.")
+            logging.info(f"Finding the general information of a job offer: {job_link}.")
+            job_description = self.wait.until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, self.JOB_ITEM_CLASSNAME)))
 
-        except:
-            pass
-        time.sleep(2)
+            job_data['Job Title'] = job_description.find_element(By.TAG_NAME, self.JOB_TITLE_TAGNAME).text
+            logging.info(f"Scraped information for {job_data['Job Title']}")
 
-    # Scraping the job description
-    job_description = driver.find_elements(By.CLASS_NAME,'jobs-description__content')
-    for description in job_description:
-        job_text = description.find_element(By.CLASS_NAME,"jobs-box__html-content").text
-        job_desc.append(job_text)
-        print(f'Scraping the Job Offer {j}')
-        print(job_text)
-        time.sleep(2)
+            job_data['Company Name'] = job_description.find_element(By.CLASS_NAME, self.COMPANY_NAME_CLASSNAME).text
+            logging.info(f"Scraped information for {job_data['Company Name']}")
 
-    # Creating the dataframe
-df = pd.DataFrame(list(zip(job_titles, company_names,
-                           company_locations, work_methods,
-                           post_dates, work_times, job_desc)),
-                  columns=['job_title', 'company_name',
-                           'company_location', 'work_method',
-                           'post_date', 'work_time', 'job_desc'])
+            job_data['Location'] = job_description.find_element(By.CLASS_NAME, self.LOCATION_CLASSNAME).text
+            logging.info(f"Scraped information for {job_data['Location']}")
 
-# Storing the data to csv file
-df.to_csv('job_offers.csv', index=False)
+            job_data['Job Type'] = job_description.find_element(By.CLASS_NAME, self.JOB_TYPE_CLASSNAME).text
+            logging.info(f"Scraped information for {job_data['Job Type']}")
+
+            job_data['Post Date'] = job_description.find_element(By.CLASS_NAME, self.POST_DATE_CLASSNAME).text
+            logging.info(f"Scraped information for {job_data['Post Date']}")
+
+            job_data['Job Description'] = job_description.find_element(By.CLASS_NAME, self.JOB_DESC_CLASSNAME).text
+            logging.info(f"Scraped information for {job_data['Job Description']}")
+
+        except Exception as e:
+            logging.error(f'Error occurred while scraping job information for {job_link}: {e}')
+        else:
+            logging.info(f"Scraping the Job Offer {job_data} DONE.")
+            return job_data
+
+    def scrape_jobs(self):
+        try:
+            self.login()
+            self.driver.get(self.JOB_SEARCH_URL)
+            job_links = self.scrape_job_links()
+            print(job_links)
+            job_data = [self.scrape_job_info(job_link) for job_link in job_links]
+            print(job_data)
+            df = pd.DataFrame(job_data)
+            df.to_csv('job_offers.csv', index=False)
+            logging.info(f'Saved data for {len(job_data)} job offers')
+        except Exception as e:
+            logging.error(f'Error occurred while scraping jobs: {e}')
+        finally:
+            self.driver.quit()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    scraper = LinkedInJobsScraper()
+    scraper.scrape_jobs()
+
